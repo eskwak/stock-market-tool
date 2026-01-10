6bef28c90099bd4a604fe9978264e1864362eed3
Eddie <ekwak30@gatech.edu>
2026-01-10 04:29:44 -0500

    Added functionality to pull 52-week high, percent off 52-week high, trailing/forward P/E ratios, and dividend yield info.

--- src/main.py
+++ src/main.py
@@ -6,21 +6,49 @@ def main():
         "aapl",
         "nvda",
         "msft",
-        "pltr"
+        "pltr",
+        "voo",
+        "vea",
+        "shld"
     ]
     
-    stock_price_dict = ticker_analysis.get_stock_prices_from_tickers(ticker_symbol_list)
-    # print(stock_price_dict)
+    stock_info_dict = ticker_analysis.yf_get_stock_info(ticker_symbol_list)
+    # Order of returns:
+    # dict[0]: currency type
+    # dict[1]: last price
+    # dict[2]: 52-week high
+    # dict[3]: percent off 52-week high (percent)
+    # dict[4]: trailing P/E ratio
+    # dict[5]: forward P/E ratio
+    # dict[6]: dividend yield (percent)
+    
+    # print(stock_info_dict)
     
     while True:
         try:
-            for ticker in stock_price_dict.keys():
-                price, currency = stock_price_dict[ticker][0], stock_price_dict[ticker][1]
-                print(f"Ticker: {ticker} | Price: {price:.2f} {currency}")
+            # All info to be put on dashboard later.
+            for ticker in stock_info_dict.keys():
+                currency_type = stock_info_dict[ticker][0]
+                last_price = stock_info_dict[ticker][1]
+                year_high = stock_info_dict[ticker][2]
+                percent_off_high = stock_info_dict[ticker][3]
+                trailing_pe = stock_info_dict[ticker][4]
+                forward_pe = stock_info_dict[ticker][5]
+                dividend_yield = stock_info_dict[ticker][6]
+                
+                print(f"${ticker}")
+                print(f"Price: ${last_price:.2f} {currency_type}")
+                print(f"52-Week High: ${year_high:.2f}")
+                print(f"Percent Off 52-Week High: {percent_off_high:.2f}%")
+                print(f"Trailing P/E: {trailing_pe}")
+                print(f"Forward P/E: {forward_pe}")
+                print(f"Dividend Yield: {dividend_yield:.2f}%\n")
+                
         except KeyboardInterrupt:
             print("Stopped monitoring.")
             break
-        print()
+
+        print("\n\n")
         time.sleep(3)
 
 if __name__ == "__main__":
--- src/ticker_analysis.py
+++ src/ticker_analysis.py
@@ -4,29 +4,81 @@ import os
 from collections import defaultdict
 
 
-def get_stock_prices_from_tickers(ticker_symbol_list: list) -> dict:
-    """Returns dictionary with {ticker: [price, currency]}.
-
+def yf_get_stock_info(ticker_symbol_list: list, interval = 3) -> dict:
+    """Returns a dictionary containing useful long-term investing information for all 
+    ticker symbols inthe ticker_symbol_list.
+    
     Args:
         ticker_symbol_list (list): A string list containing ticker symbols.
+        interval (int): a sleep interval used to avoid DDOS bans from yahoo.
+
+    Returns:
+        dict: Returns dictionary with 
+        {
+            ticker: [
+                currency type,
+                last price,
+                52-week high,
+                percent off 52-week high (percent),
+                trailing P/E ratio,
+                forward P/E ratio,
+                dividend yield (percent)
+            ]
+        }
     """
-    stock_price_dict = defaultdict(list)
+    stock_info_dict = defaultdict(list)
     
-    for ticker_symbol in ticker_symbol_list:
-        price_and_currency = yf_poll_stock_price(ticker_symbol)
-        stock_price_dict[ticker_symbol] = price_and_currency
+    for ticker_string in ticker_symbol_list:
+        ticker = yf.Ticker(ticker_string)
+        currency_type = ticker.fast_info["currency"]
+        last_price = ticker.fast_info["last_price"]
+        year_high = ticker.fast_info["year_high"]
+        percent_off_high = ((last_price - year_high) / year_high) * 100.0
+        
+        # .info is used to avoid crashes for tickers where certain information may not be available.
+        ticker_info = ticker.info
+        trailing_pe = ticker_info.get("trailingPE")
+        forward_pe = ticker_info.get("forwardPE")
+        dividend_yield = ticker_info.get("dividendYield", 0)
+        
+        stock_info_dict[ticker_string] = [
+            currency_type,
+            last_price,
+            year_high,
+            percent_off_high,
+            trailing_pe,
+            forward_pe,
+            dividend_yield
+        ]
+        
+        time.sleep(interval)
     
-    return stock_price_dict
+    return stock_info_dict
+    
+    
+# def get_stock_prices_from_tickers(ticker_symbol_list: list) -> dict:
+#     """Returns dictionary with {ticker: [price, currency]}.
+
+#     Args:
+#         ticker_symbol_list (list): A string list containing ticker symbols.
+#     """
+#     stock_price_dict = defaultdict(list)
+    
+#     for ticker_symbol in ticker_symbol_list:
+#         price_and_currency = yf_poll_stock_price(ticker_symbol)
+#         stock_price_dict[ticker_symbol] = price_and_currency
+    
+#     return stock_price_dict
 
 
-def yf_poll_stock_price(ticker_symbol, interval = 3) -> list:
-    """Returns [price, currency] for a specific ticker.
+# def yf_poll_stock_price(ticker_symbol, interval = 3) -> list:
+#     """Returns [price, currency] for a specific ticker.
 
-    Args:
-        ticker_symbol (str): A string representation of a ticker symbol ($msft)
-    """
-    ticker = yf.Ticker(ticker_symbol)
-    last_price = ticker.fast_info["last_price"]
-    currency = ticker.fast_info["currency"]
-    time.sleep(interval)
-    return [last_price, currency]
+#     Args:
+#         ticker_symbol (str): A string representation of a ticker symbol ($msft)
+#     """
+#     ticker = yf.Ticker(ticker_symbol)
+#     last_price = ticker.fast_info["last_price"]
+#     currency = ticker.fast_info["currency"]
+#     time.sleep(interval)
+#     return [last_price, currency]
